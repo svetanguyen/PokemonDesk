543c0f586e1d6e7da10bfd2d89c203cdd42c7e8e
svetanguyen <matematicaforeber@gmail.com>
2020-11-29 01:56:02 +0700

    avoid the overlapping of players

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ import { pokemons } from "./pokemons.js";
 import {random} from './utilities.js';
 const controlPart = document.querySelector('.control');
 const startBtn = document.getElementById('Start');
-// const pikachu = pokemons.find(item => item.name === 'Pikachu');
-let randomPlayerOne = pokemons[random(pokemons.length - 1)];
+let index1 = random(pokemons.length - 1);
+let randomPlayerOne = pokemons[index1];
+pokemons.splice(index1, 1);
 let randomPlayerTwo = pokemons[random(pokemons.length - 1)];
 const resetGame = document.getElementById('Reset');
-// console.log(randomPlayer.img);
+console.log(randomPlayerOne.img);
 /*никак не выходит прогрузить картинки. если
- выводить их через переменную randomPlayer виден путь, но если через саму 
+ выводить их через переменную randomPlayerOne виден путь, но если через саму 
  переменную player1 - уже нет, хотя имя, к примеру, грузится :C */
 
 let player1 = new Game({
@@ -17,6 +18,7 @@ let player1 = new Game({
 	selectors: 'player1'
 
 });
+
 console.log(player1.img);
 let player2 = new Game({
 	...randomPlayerTwo,
