e1509396360b49f711fdbed18bb6dfed10f6d891
svetanguyen <matematicaforeber@gmail.com>
2020-12-01 00:16:33 +0700

    get pokemons server

--- main.js
+++ main.js
@@ -1,52 +1,71 @@
 import Game from "./constructor.js";
-import { pokemons } from "./pokemons.js";
 import {random, resetLog} from './utilities.js';
 const controlPart = document.querySelector('.control');
 const startBtn = document.getElementById('Start');
-let index1 = random(pokemons.length - 1);
-let randomPlayerOne = pokemons[index1];
-// this is to avoid overlapping of characters
-pokemons.splice(index1, 1);
-let randomPlayerTwo = pokemons[random(pokemons.length - 1)];
 const resetGame = document.getElementById('Reset');
-console.log(randomPlayerOne.img);
-
-let player1 = new Game({
-	...randomPlayerOne,
-	selectors: 'player1'
-
-});
-console.log(player1);
-console.log(player1.img);
-let player2 = new Game({
-	...randomPlayerTwo,
-	selectors: 'player2'
-
-});
 
 
 
-startBtn.addEventListener('click', () => {
-	player1.startGame(startBtn, player2, player1);
-	
-});
-console.log(resetGame);
+class Gameflow {
+	getPokemons = async () => {
+		const response = await fetch('https://reactmarathon-api.netlify.app/api/pokemons');
+		const body = await response.json();
+		return body;
+	};
 
-resetGame.addEventListener('click', () => {
-	resetLog();
-	randomPlayerTwo = pokemons[random(pokemons.length - 1)];
-	player2 = new Game({
-		...randomPlayerTwo,
-		selectors: 'player2'
+	start = async () => {
+		const pokemons = await this.getPokemons();
+		let index1 = random(pokemons.length - 1);
+		let randomPlayerOne = pokemons[index1];
+		pokemons.splice(index1, 1);
+		let randomPlayerTwo = pokemons[random(pokemons.length - 1)];
+		console.log(pokemons);
+		let player1 = new Game({
+			...randomPlayerOne,
+			selectors: 'player1'
 
-	});
-	player1.resetHP();
-	player2.resetHP();
-	player1.resetMessage();
-	player2.resetMessage();
-	player1.startGame(resetGame, player2, player1);
+		});
+		// console.log(player1);
+		// console.log(player1.img);
+		let player2 = new Game({
+			...randomPlayerTwo,
+			selectors: 'player2'
 
-});
+		});
+
+
+
+		startBtn.addEventListener('click', () => {
+			player1.startGame(startBtn, player2, player1);
+			
+		});
+		console.log(resetGame);
+
+		resetGame.addEventListener('click', () => {
+			resetLog();
+			randomPlayerTwo = pokemons[random(pokemons.length - 1)];
+			player2 = new Game({
+				...randomPlayerTwo,
+				selectors: 'player2'
+
+			});
+			player1.resetHP();
+			player2.resetHP();
+			player1.resetMessage();
+			player2.resetMessage();
+			player1.startGame(resetGame, player2, player1);
+
+		});
+	};
+};
+
+// this is to avoid overlapping of characters
+
+
+// console.log(randomPlayerOne.img);
+
+const game = new Gameflow();
+game.start();
 
 // console.log(pikachu);
 
